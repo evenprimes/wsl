e7b46f16c45bc94f5dc001dff11e7d8c845c9063
Chris Cioffi <chris@evenprimes.com>
2022-12-22 11:34:17 -0500

    Add running process notes

--- wsl_install.sh
+++ wsl_install.sh
@@ -1,13 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-cp --verbose ./mosquitto_sub ~/.config/mosquitto_sub
-cp --verbose ./bashrc_wsl ~/.bashrc
-
-[ ! -d ~/bin ] && mkdir ~/bin || echo ~/bin/ exists
-[ ! -d ~/.1password ] && mkdir ~/.1password || echo ~/.1password/ exists
+# Setup colors
+green='\e[0;32m'
+yellow='\e[0;33m'
+reset='\033[0m'
 
 SCRIPTS="update_omp.sh"
 
+echo -e "${green}Copying config files"
+cp --verbose ./mosquitto_sub ~/.config/mosquitto_sub
+cp --verbose ./bashrc_wsl ~/.bashrc
+
+echo -e "${green}Creating directories"
+[ ! -d ~/bin ] && mkdir ~/bin || echo ~/bin/ exists
+[ ! -d ~/.1password ] && mkdir ~/.1password || echo ~/.1password/ exists
+
+echo -e "${green}Installing scripts"
 for i in $SCRIPTS; do
     cp --verbose $i ~/bin/
     chmod +x ~/bin/$i
